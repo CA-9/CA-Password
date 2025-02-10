333411919b7a5a01de46650c354aa4f6421616d2
CA.1 <mohammad92007@gmail.com>
2025-02-10 17:26:38 +0400

    config.lua

---
+++ config.lua
@@ -0,0 +1,26 @@
+Cadev = {}
+
+Cadev.CoreName = 'qb-core'
+
+Cadev.Locations = {
+    {
+        id = 1,
+        name = "Ca.dev",
+        coords = vector3(432.92, -586.2, 28.5),
+        distance = 5,
+        interactDst = 3,
+        password = "pd-heli",
+        event = "CA-ALERT",
+        uitext = "[E] Heli Garage",
+    },
+    {
+        id = 2,
+        name = "Ca.dev2",
+        coords = vector3(481.55, -981.9, 41.01),
+        distance = 5,
+        interactDst = 3,
+        password = "ca.1",
+        event = "CA-ALERT",
+        uitext = "[E] Heli Garage",
+    },
+}
